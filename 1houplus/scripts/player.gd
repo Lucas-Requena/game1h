679adc992347663ec11c17fdc62457947f9f81da
lrequena-iut90 <lucas.requena.foch@gmail.com>
2025-01-29 00:01:24 +0100

    #

--- 1houplus/scripts/player.gd
+++ 1houplus/scripts/player.gd
@@ -2,10 +2,6 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _ready():
-    # Positionner le joueur au centre de l'écran au démarrage
-    position = get_viewport_rect().size / 2
-
 func _physics_process(delta: float) -> void:
     # Obtenir les entrées du joueur
     var direction = Vector2.ZERO
@@ -15,12 +11,10 @@ func _physics_process(delta: float) -> void:
     # Normaliser le vecteur pour éviter une vitesse plus rapide en diagonale
     if direction.length() > 0:
         direction = direction.normalized()
-        # Retourner le sprite selon la direction
-        if direction.x < 0:
-            $Sprite2D.flip_h = true
-        elif direction.x > 0:
-            $Sprite2D.flip_h = false
-            
+        $AnimatedSprite2D.play("walk")
+    else:
+        $AnimatedSprite2D.play("idle")
+        
     # Appliquer le mouvement
     velocity = direction * SPEED
-    move_and_slide() 
+    move_and_slide()
